48de87ed738046dd9ef4e85679992ccc5c3b0681
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2016-02-16 11:39:16 +0300

    add tests for unimplemented methods behaviour for childs of ITLinkNode class

--- Example/Tests/ITLinkNodeTests.m
+++ Example/Tests/ITLinkNodeTests.m
@@ -6,10 +6,47 @@
 //  Copyright © 2016 Alex Rudyak. All rights reserved.
 //
 
+@interface ITLinkNodeChild : ITLinkNode
+@end
+
+@implementation ITLinkNodeChild
+@end
+
 SpecBegin(ITLinkNodeTests);
 
 describe(@"instance methods", ^{
-         });
+    context(@"which haven't provide its implementation should raise exception", ^{
+        __block ITLinkNodeChild *childNode;
+
+        beforeEach(^{
+            childNode = [[ITLinkNodeChild alloc] initWithModuleName:@"ChildModule" router:nil];
+        });
+
+        it(@"for flatten method", ^{
+            expect(^{
+                __unused id<ITLinkNode> flatteNode = [childNode flatten];
+            }).to.raise(ITUnimplementedMethod);
+        });
+
+        it(@"for forward module invocation method", ^{
+            expect(^{
+                __unused NSInvocation *invocation = [childNode forwardModuleInvocation];
+            }).to.raise(ITUnimplementedMethod);
+        });
+
+        it(@"for backward module invocation method", ^{
+            expect(^{
+                __unused NSInvocation *invocation = [childNode backwardModuleInvocation];
+            }).to.raise(ITUnimplementedMethod);
+        });
+
+        it(@"for copy method", ^{
+            expect(^{
+                __unused id<ITLinkNode> copiedNode = [childNode copy];
+            }).to.raise(ITUnimplementedMethod);
+        });
+    });
+});
 
 describe(@"cluster methods", ^{
     it(@"should create proper instances", ^{
